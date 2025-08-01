5b8b549abe15051f2256f1b2e028f975ea433ece
Altroplis98 <59132603+Altroplis98@users.noreply.github.com>
2025-07-31 19:56:50 -0500

    Initial commit
    Initial commit

---
+++ firstPythonCode.py
@@ -0,0 +1,8 @@
+#Welcome to Learning, Time to Man up
+import time
+print("Hello, Altroplis!")
+time.sleep(1)
+print('Remember nothing changes, if nothing changes!')
+name = input("What is your name? ")
+type(name)
+print(name + " is in love with Alex!")
---
+++ secondPython.py
@@ -0,0 +1,5 @@
+#Welcome to Learning, Time to Man up
+import time
+print("Hello, Altroplis!")
+time.sleep(1)
+print('Remember nothing changes, if nothing changes!')
